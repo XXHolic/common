9da665927a04486b47571ded14093f58f5a47435
thy <972143891@qq.com>
2019-01-06 14:41:15 +0800

    feat: add new function in tool

--- packages/tool/index.js
+++ packages/tool/index.js
@@ -1,11 +1,11 @@
-var Tool={};
+var Tool = {};
 
 /**
  * judge device type(pc or phone)
  * return string
  * blog link:http://www.cnblogs.com/babycool/p/3583114.html
  */
-Tool.getDeviceType = function() {
+Tool.getDeviceType = function () {
   var userAgent = navigator.userAgent.toLowerCase();
   var isIpad = userAgent.match(/ipad/i) == "ipad";
   var isIphoneOs = userAgent.match(/iphone os/i) == "iphone os";
@@ -27,61 +27,79 @@ Tool.getDeviceType = function() {
  * ie 不支持参数默认赋值，link:https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Functions/Default_parameters
  */
 Tool.Event = {
-  addHandler: function(element, type, handler, useCapturing) {
-    if(element.addEventListener) {
+  addHandler: function (element, type, handler, useCapturing) {
+    if (element.addEventListener) {
       // IE9、Firefox、Safari、Chrome 和Opera 支持DOM2 级事件处理程序。
       element.addEventListener(type, handler, useCapturing || false);
-    } else if(element.attachEvent) {
+    } else if (element.attachEvent) {
       // IE<9
       element.attachEvent('on' + type, handler);
     } else {
       element['on' + type] = handler;
     }
   },
-  removeHandler: function(element, type, handler,useCapturing) {
-    if(element.removeEventListener) {
+  removeHandler: function (element, type, handler, useCapturing) {
+    if (element.removeEventListener) {
       // IE9、Firefox、Safari、Chrome 和Opera 支持DOM2 级事件处理程序。
       element.removeEventListener(type, handler, useCapturing || false);
-    } else if(element.detachEvent) {
+    } else if (element.detachEvent) {
       // IEe<9
       element.detachEvent('on' + type, handler);
     } else {
       element['on' + type] = null;
     }
   },
-  getEvent: function(event) {
+  getEvent: function (event) {
     return event ? event : window.event;
   },
   // 阻止事件传播
-  stopPropagation: function(event) {
-    if (event.stopPropagation){
+  stopPropagation: function (event) {
+    if (event.stopPropagation) {
       event.stopPropagation();
     } else {
       event.cancelBubble = true;
     }
   },
   // 阻止默认行为
-  stopDefault: function(event) {
-    if (event.preventDefault){
+  stopDefault: function (event) {
+    if (event.preventDefault) {
       event.preventDefault();
     } else {
-        event.returnValue = false;
+      event.returnValue = false;
     }
   }
 }
 
 // 获取一个随机数字符串，用来做对比
-Tool.getRandomNum = function(len) {
+Tool.getRandomNum = function (len) {
   var numString = String(Math.random());
   if (len) {
-    return numString.substring(0,len);
+    return numString.substring(0, len);
   } else {
     return numString;
   }
 }
 
-Tool.console = function(str) {
-  console.log(str);
-}
+/*根据当前URL或者传入URL获取参数值*/
+Tool.getParam = function (url) {
+  var param, theRequest = {};	//url = str,
+  !url && (url = window.location.href);
+  if (url.indexOf("?") !== -1) {
+    var str = url.split('?')[1];
+    var strs = str.split("&");
+    for (var i = 0, len = strs.length; i < len; i++) {
+      param = strs[i].split("=");
+      theRequest[param[0]] = decodeURIComponent(param[1]);
+    }
+  }
+  return theRequest;
+};
+//根据传入id获取指定URL上的指定参数
+Tool.getQueStr = function (ref) {
+  var reg = new RegExp("(^|&)" + ref + "=([^&]*)(&|$)");
+  var r = window.location.search.substr(1).match(reg);
+  if (r !== null) return unescape(r[2]);
+  return null;
+};
 
 export default Tool;
